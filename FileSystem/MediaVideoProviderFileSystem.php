de95dc786d8425304d4bfc042cc768f2a8c181af
Damiano Ciarla <damiano.ciarla@gmail.com>
2014-11-23 11:33:46 +0100

    Update file system recover thumbnail

--- FileSystem/MediaVideoProviderFileSystem.php
+++ FileSystem/MediaVideoProviderFileSystem.php
@@ -32,15 +32,31 @@ class MediaVideoProviderFileSystem implements FileSystemInterface
     public function upload(FileInfoInterface $fileInfo, MediaProviderInterface $mediaProvider, $filePath)
     {
         if (null !== $videoInfo = $this->http->getInfoUrl($mediaProvider->getProvider(), $fileInfo->getTmpName())) {
-            $thumbPath = $this->pathGenerator->generate($mediaProvider).DIRECTORY_SEPARATOR.
-                         md5(uniqid()).'.'.pathinfo($videoInfo['thumbnail_url'], PATHINFO_EXTENSION);
 
-            $thumb = file_get_contents($videoInfo['thumbnail_url']);
-            file_put_contents($thumbPath, $thumb);
+            $thumbnailUrl    = null;
+            $thumbnailWidth  = null;
+            $thumbnailHeight = null;
+            $thumbnailPath   = null;
+            $embedUrl        = null;
 
-            $embedUrl = null;
+            if (!empty($videoInfo['thumbnail_url'])) {
+                $thumbnailUrlParsed = parse_url($videoInfo['thumbnail_url']);
+                $thumbnailUrl = join('', array(
+                    isset($thumbnailUrlParsed['scheme']) ? $thumbnailUrlParsed['scheme'].'://' : 'http://',
+                    $thumbnailUrlParsed['host'],
+                    $thumbnailUrlParsed['path']
+                ));
 
-            if (preg_match('/<iframe.*src=\"(.*)\".*><\/iframe>/isU', $videoInfo['html'], $matches)) {
+                $thumbnailWidth = $videoInfo['thumbnail_width'];
+                $thumbnailHeight = $videoInfo['thumbnail_height'];
+
+                $thumbnailPath = $this->pathGenerator->generate($mediaProvider).DIRECTORY_SEPARATOR.md5(uniqid()).'.'.pathinfo($thumbnailUrl, PATHINFO_EXTENSION);
+
+                $thumb = file_get_contents($videoInfo['thumbnail_url']);
+                file_put_contents($thumbnailPath, $thumb);
+            }
+
+            if (preg_match('/<iframe.*src=\"(.*)\".*>.*<\/iframe>/isU', $videoInfo['html'], $matches)) {
                 $embedUrl = $matches[1];
             }
 
@@ -49,12 +65,12 @@ class MediaVideoProviderFileSystem implements FileSystemInterface
             $videoProviderInfo->setVideoTitle($videoInfo['title']);
             $videoProviderInfo->setWidth($videoInfo['width']);
             $videoProviderInfo->setHeight($videoInfo['height']);
-            $videoProviderInfo->setAuthorName($videoInfo['author_name']);
-            $videoProviderInfo->setAuthorUrl($videoInfo['author_url']);
-            $videoProviderInfo->setThumbnailUrl($videoInfo['thumbnail_url']);
-            $videoProviderInfo->setThumbnailWidth($videoInfo['thumbnail_width']);
-            $videoProviderInfo->setThumbnailHeight($videoInfo['thumbnail_height']);
-            $videoProviderInfo->setThumbnailPath($thumbPath);
+            $videoProviderInfo->setAuthorName(!empty($videoInfo['author_name']) ? $videoInfo['author_name'] : null);
+            $videoProviderInfo->setAuthorUrl(!empty($videoInfo['author_url']) ? $videoInfo['author_url'] : null);
+            $videoProviderInfo->setThumbnailUrl($thumbnailUrl);
+            $videoProviderInfo->setThumbnailWidth($thumbnailWidth);
+            $videoProviderInfo->setThumbnailHeight($thumbnailHeight);
+            $videoProviderInfo->setThumbnailPath($thumbnailPath);
             $videoProviderInfo->setEmbedUrl($embedUrl);
             $videoProviderInfo->setHtml($videoInfo['html']);
 
